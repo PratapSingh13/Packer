cf4c8b8632a33bc0885174d1747eb6599d8816d3
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-14 10:36:34 +0530

    Update admin_script.sh

--- admin_script.sh
+++ admin_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #Creating Code directory
 createDirectory() {
-    echo "Creating ${ENV} dir"
-    sudo mkdir -p /var/www/html/${ENV}.docasap.com
-    sudo mkdir -p /var/www/html/${ENV}-laravel-logs
-    sudo mkdir -p /var/www/html/${ENV}.docasap.com
+    sudo mkdir -p /var/www/html/${ENVIRONMENT}.docasap.com
+    sudo mkdir -p /var/www/html/${ENVIRONMENT}-laravel-logs
+    sudo mkdir -p /var/www/html/sync${ENVIRONMENT}.docasap.com
+    sudo mkdir -p /da/log/audit/${ENVIRONMENT}-admin-portal
+    sudo chown apache:apache /da/log/audit/${ENVIRONMENT}-admin-portal -R
+    sudo mkdir -p /da/log/audit/${ENVIRONMENT}-admin-portal
+    sudo chmod 771 /da/log/audit/${ENVIRONMENT}-admin-portal/
 }
 
 echo "Creating Directories"
