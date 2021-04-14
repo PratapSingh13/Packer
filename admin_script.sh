6b32237747c90df442bb13df98feffb33d7e04bd
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-04-14 14:56:45 +0530

    Update admin_script.sh

--- admin_script.sh
+++ admin_script.sh
@@ -10,5 +10,62 @@ createDirectory() {
     sudo chmod 771 /da/log/audit/${ENVIRONMENT}-admin-portal/
 }
 
+copySSlFiles() {
+    sudo aws s3 cp s3://da-app-configuration/testing/web/certs/rds-combined-ca-bundle.pem /var/www/certs/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/certs/rds-ca-2015-us-east-1.pem /var/www/certs/
+
+}
+
+copyPackageConfigurationFiles() {
+    sudo aws s3 cp s3://da-app-configuration/testing/web/php/php.ini /etc/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/php/newrelic.ini /etc/php.d/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/php/opcache.ini /etc/php.d/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/php/opcache-default.blacklist /etc/php.d/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/apache/httpd.conf /etc/httpd/conf/
+    sudo aws s3 cp s3://da-app-configuration/testing/web/apache/ssl.conf /etc/httpd/conf.d/
+}
+
+copyTLSCertificates() {
+    echo "Copying TLS certs"
+}
+
+copyComposer() {
+    cd /usr/local/bin
+    sudo aws s3 cp s3://da-app-configuration/testing/web/php/composer .
+    sudo chmod 775 composer
+}
+
+copyApplicationConfigFiles() {
+    sudo aws s3 cp s3://da-app-configuration/testing/web/application/.env /var/www/html/testing.docasap.com/
+}
+
+settingPermissions() {
+    sudo mkdir -p /var/www/html/testing.docasap.com/storage/cronjob_logs
+    sudo chmod 771 /var/www/html/ -R
+    cd /var/www/html/testing.docasap.com/
+    sudo cp /usr/local/bin/composer /var/www/html/testing.docasap.com/composer.phar
+    sudo chmod 775 /var/www/html/testing.docasap.com -R
+    sudo chmod 775 composer.phar
+    sudo chown apache:apache -R .
+}
+
 echo "Creating Directories"
 createDirectory
+
+echo "Copying SSl files from S3"
+copySSlFiles
+
+echo "Copying ini files and conf files from S3 for CRON application"
+copyPackageConfigurationFiles
+
+echo "Copying certificates from S3"
+copyTLSCertificates
+
+echo "Copying Composer files from S3 for CRON application"
+copyComposer
+
+echo "Copying application config files from S3 for CRON application"
+copyApplicationConfigFiles
+
+echo "Setting few permissions"
+settingPermissions
